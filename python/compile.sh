b2cf5e1929918890153a71b80ca39a918104a088
Sebastian Schwalbe <48414071+theonov13@users.noreply.github.com>
2019-04-12 20:33:07 +0200

    Update compile.sh

--- python/compile.sh
+++ python/compile.sh
@@ -1,3 +1,4 @@
-#cp ../src/fodMC_alex.f90 fodmc.f90
+# cp ../src/fodMC_alex.f90 fodmc.f90
+# cp ../src/xx_database_xx . 
 # convert fortran rountine to a .so object which can be used by python 
 f2py -c -m fodmc fodmc.f90
