9f76d8ad95fd6ffe442c8ca06156fe169086e1e0
kaitrepte <trept1k@cmich.edu>
2020-05-21 11:31:40 -0400

    KT: New structure, update

---
+++ fodMC/examples/fortran/run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Author: S. Schwalbe 
+# Date: 21.05.2020 
+# you might need to change the paths according to your needs
+cp ../../src/fortran/xx_database_xx . 
+#$fodmc/src/fodMC
+../../src/fortran/fodMC_motif
