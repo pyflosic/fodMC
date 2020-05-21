25ec401a42435d928da3e4053f9d065779f61209
kaitrepte <trept1k@cmich.edu>
2020-05-21 11:45:06 -0400

    KT: Further restructuring

--- fodMC/examples/fortran/run.sh
+++ fodMC/examples/fortran/run.sh
@@ -3,6 +3,6 @@
 # Author: S. Schwalbe 
 # Date: 21.05.2020 
 # you might need to change the paths according to your needs
-cp ../../src/fortran/xx_database_xx . 
+cp ../../lib/xx_database_xx . 
 #$fodmc/src/fodMC
-../../src/fortran/fodMC_motif
+../../lib/fodMC_motif
