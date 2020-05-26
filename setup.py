b2896e443488fb51f296e288ffa32663b905f931
theonov13 <theonov13@gmail.com>
2020-05-26 11:28:16 +0200

    change setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 import setuptools
 from numpy.distutils.core import Extension, setup
 
+with open("README.md", "r") as fh:
+     name="fodMC",                                                                          
+     long_description = fh.read()  
+
 setup(
    name="fodMC",
-   version="1.0",
+   version="1.0.1",
    author="Kai Trepte",
    author_email="kai.trepte1987@gmail.com",
-   description="Fermi-orbital descriptor generator",
+   description="Fermi-orbital descriptor Monte-Carlo",
+   long_description=long_description,
+   long_description_content_type="text/markdown",
+   include_package_data=True,
    packages = setuptools.find_packages(),
    ext_modules=[Extension(name='fodmc', sources=['fodMC/lib/fodmc.f90'], f2py_options=['--quiet'])]
 )
